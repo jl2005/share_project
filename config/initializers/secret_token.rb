d0c1dbd6d71feac949ae9fc2cc2fd3519446946f
jl2005 <jiaolongdy@163.com>
2013-12-21 10:03:01 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShareProject::Application.config.secret_key_base = 'beed080e63d5b9f899f3e3287f5a28b1b915016b347a6c86e933573a85d2b8c437b6655c4c385149aeac2afbda104f3e36c4d4a749a62ad85491364c3161a05f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShareProject::Application.config.session_store :cookie_store, key: '_share_project_session'
